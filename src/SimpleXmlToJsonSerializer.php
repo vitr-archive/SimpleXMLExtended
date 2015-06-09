a3038d15e19d758aaf92b39f0c00fb615f8a60fc
Vitaliy.Ryepnoy <Vitaliy.Ryepnoy@dealersolutions.com.au>
2015-06-09 16:50:11 +1000

    improved detection of 'tricky' nodes

--- src/SimpleXmlToJsonSerializer.php
+++ src/SimpleXmlToJsonSerializer.php
@@ -1,22 +1,1 @@
-<?php
-/*
- * Keeps the array structure for simpleXML nodes with only one child
- * during json encoding
- *
- * usage:
- *
- * $mySimpleXML = new SimpleXmlToJsonSerializer($xmlString);
- * json_encode($mySimpleXML);
- *
- */
-class SimpleXmlToJsonSerializer extends SimpleXMLElement implements JsonSerializable
-{
-    public function jsonSerialize()
-    {
-        $value = (array)$this;
-        //if (count($value) == 1 and gettype(reset($value)) != 'array')
-        if (count($value) == 1)
-            return [key($value) => [(array)reset($value)]];
-        return $value;
-    }
-}
+<?php/* * Keeps the array structure for simpleXML nodes with only one child * during json encoding * * usage: * * $mySimpleXML = new SimpleXmlToJsonSerializer($xmlString); * json_encode($mySimpleXML); * */class SimpleXmlToJsonSerializer extends SimpleXMLElement implements JsonSerializable{    public function jsonSerialize()    {        $value = (array)$this;        if (count($value) == 1 and gettype(reset($value)) != 'array')            return [key($value) => [(array)reset($value)]];        return $value;    }}

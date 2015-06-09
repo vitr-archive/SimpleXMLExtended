7fb59ab4ae63e79afaa9daf6e2b5d79960a0f66d
Vitaliy.Ryepnoy <Vitaliy.Ryepnoy@dealersolutions.com.au>
2015-06-09 16:18:03 +1000

    a concern added, needs more tests

--- src/SimpleXmlToJsonSerializer.php
+++ src/SimpleXmlToJsonSerializer.php
@@ -14,6 +14,7 @@ class SimpleXmlToJsonSerializer extends SimpleXMLElement implements JsonSerializ
     public function jsonSerialize()
     {
         $value = (array)$this;
+        //if (count($value) == 1 and gettype(reset($value)) != 'array')
         if (count($value) == 1)
             return [key($value) => [(array)reset($value)]];
         return $value;
